9a985957653782324077c040829da9871586d1af
Thor Tudal Lauridsen <thtl@itu.dk>
2021-09-06 18:09:24 +0200

    Implemented error handling

--- BDSA/LeapYearService.cs
+++ BDSA/LeapYearService.cs
@@ -5,15 +5,25 @@ namespace Prime.Services
     public class LeapYearService
     {
         public static void Main(string[] args) {
+
+            Console.WriteLine("Please enter a year:");
             string value = Console.ReadLine();
 
             var leapYearService = new LeapYearService();
-            bool isItLeapYear = leapYearService.IsLeapYear(Convert.ToInt32(value));
-
-            if (isItLeapYear) {
-                Console.WriteLine("yay");
-            } else {
-                Console.WriteLine("nay");
+            try {
+                Int32 number = Convert.ToInt32(value);
+                bool isItLeapYear = leapYearService.IsLeapYear(number);
+                if (number >= 1582) {
+                    if (isItLeapYear) {
+                        Console.WriteLine("yay");
+                    } else {
+                        Console.WriteLine("nay");
+                    }
+                } else {
+                    Console.WriteLine("Please insert a number higher or equal to 1582!");
+                }
+            } catch (FormatException _) {
+                Console.WriteLine("This only work for numbers!");
             }
         }
 
