0023ab8deb9ac6666be3d475b375a1269675c822
Thor Tudal Lauridsen <thtl@itu.dk>
2021-09-06 21:45:31 +0200

    Add new test

--- BDSA.Tests/LeapYearTest.cs
+++ BDSA.Tests/LeapYearTest.cs
@@ -52,5 +52,35 @@ namespace Prime.UnitTests.Services
 
             Assert.True(_leapYearService.IsLeapYear(value));
         }
+
+        [Fact]
+        public void IsLeapYear_Below1582()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("1581");
+            Console.SetIn(input);
+
+            LeapYearService.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("Please insert a number higher or equal to 1582!", output);
+        }
+
+        [Fact]
+        public void IsLeapYear_InvalidInteger()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            var input = new StringReader("AAAAAAA");
+            Console.SetIn(input);
+
+            LeapYearService.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("This only work for numbers!", output);
+        }
     }
 }
